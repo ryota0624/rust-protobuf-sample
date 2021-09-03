bdd72d05fbf23e2591fb58c8584ede95953ac837
ryota0624 <tennis.suta6@gmail.com>
2021-09-03 15:26:49 +0900

    :hammer: clean build.rs

---
+++ build.rs
@@ -0,0 +1,41 @@
+use std::ffi::OsString;
+use std::fs;
+use std::fs::ReadDir;
+use std::fs::{DirBuilder, DirEntry};
+use std::io;
+use std::io::prelude::*;
+use std::io::{Error, Result};
+use std::iter::FlatMap;
+use std::path::Path;
+use std::path::PathBuf;
+
+use prost_build;
+
+fn find_file(dir: &Path, check: &dyn Fn(&Path) -> bool) -> io::Result<Vec<PathBuf>> {
+    if dir.is_dir() {
+        let mut dir = fs::read_dir(dir)?;
+        dir.try_fold(Vec::new(), |paths: Vec<_>, entry| {
+            let entry = entry?;
+            let path = entry.path();
+            let files = find_file(path.as_path().clone(), check)?;
+            Ok([paths, files].concat())
+        })
+    } else {
+        let files: Vec<_> = if check(dir) {
+            vec![dir.to_path_buf()]
+        } else {
+            vec![]
+        };
+        Ok(files)
+    }
+}
+
+fn main() -> Result<()> {
+    let proto_files = find_file(Path::new("src/proto"), &|path| {
+        let ext = path.extension().map(std::ffi::OsStr::to_str).flatten();
+        ext == Some("proto")
+    })?;
+    prost_build::compile_protos(&proto_files, &vec!["src/"])?;
+
+    Ok(())
+}
