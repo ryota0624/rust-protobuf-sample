005310325a5aa9ef4bb024bbd6ac6d64f708ea9c
ryota0624 <tennis.suta6@gmail.com>
2021-09-03 23:02:04 +0900

    impl by Box

--- src/event_publisher.rs
+++ src/event_publisher.rs
@@ -3,13 +3,13 @@ use prost::Message;
 pub enum Error {}
 
 pub trait EventPublisher {
-    fn publish<E>(self, e: &E, converter: &dyn EventConverter<E>) -> Result<(), Error>;
+    fn publish<E>(&self, e: &E, converter: &dyn EventConverter<E>) -> Result<(), Error>;
 }
 
 pub struct ConsoleEventPublisher;
 
 impl EventPublisher for ConsoleEventPublisher {
-    fn publish<E>(self, e: &E, converter: &dyn EventConverter<E>) -> Result<(), Error> {
+    fn publish<E>(&self, e: &E, converter: &dyn EventConverter<E>) -> Result<(), Error> {
         let bytes = converter.to_bytes(e);
         println!("ConsoleEventPublisher.publish {:?}!", bytes);
         Ok(())
@@ -17,7 +17,7 @@ impl EventPublisher for ConsoleEventPublisher {
 }
 
 pub struct InmemoryEventPublisher<M: Fn(&Vec<u8>)> {
-    listeners: Vec<M>
+    listeners: Vec<Box<M>>
 }
 
 impl<M: Fn(&Vec<u8>)>  InmemoryEventPublisher<M> {
@@ -28,20 +28,33 @@ impl<M: Fn(&Vec<u8>)>  InmemoryEventPublisher<M> {
     }
 }
 
+pub struct RemoveListenerKey(usize);
+pub struct AddResult<M: Fn(&Vec<u8>)>(InmemoryEventPublisher<M>, RemoveListenerKey);
+
 impl<M: Fn(&Vec<u8>)> InmemoryEventPublisher<M> {
-    pub fn add_listener(mut self, listener: M) -> Self {
+    pub fn add_listener(mut self, listener: Box<M>) -> AddResult<M> {
+        let len = self.listeners.len();
         self.listeners.push(listener);
-        self
+        AddResult(self, RemoveListenerKey(len))
+    }
+}
+
+impl<M: Fn(&Vec<u8>)> AddResult<M> {
+    pub fn add_listener(self, listener: Box<M>) -> AddResult<M> {
+        self.0.add_listener(listener)
+    }
+
+    pub fn publisher(self) -> InmemoryEventPublisher<M> {
+        self.0
     }
 }
 
 impl<M: Fn(&Vec<u8>)> EventPublisher for InmemoryEventPublisher<M> {
-    fn publish<E>(self, e: &E, converter: &dyn EventConverter<E>) -> Result<(), Error> {
+    fn publish<E>(&self, e: &E, converter: &dyn EventConverter<E>) -> Result<(), Error> {
         let bytes = converter.to_bytes(e);
-        // println!("ConsoleEventPublisher.publish {:?}!", bytes);
-        for listener in self.listeners {
+        self.listeners.iter().for_each(|listener| {
             listener(&bytes);
-        }
+        });
         Ok(())
     }
 }
--- src/event_subscriber.rs
+++ src/event_subscriber.rs
@@ -1,6 +1,6 @@
 use std::marker::PhantomData;
 
-use prost::Message;
+use prost::{Message, DecodeError};
 
 pub trait EventSubscriber {
     fn handle(&self, event_bytes: &Vec<u8>);
@@ -10,7 +10,7 @@ pub struct ProstMessageEventSubscriber<M, F> where
     M: Message,
     F: Fn(&M)
 {
-    handle_prost_event: F,
+    handle_prost_event: Box<F>,
     _m: PhantomData<fn() -> M>,
 }
 
@@ -18,7 +18,7 @@ impl<M, F> ProstMessageEventSubscriber<M, F> where
     M: Message,
     F: Fn(&M)
 {
-    pub fn new(handle: F) -> Self {
+    pub fn new(handle: Box<F>) -> Self {
         ProstMessageEventSubscriber {
             handle_prost_event: handle,
             _m: PhantomData::default(),
@@ -32,7 +32,14 @@ impl<M, F> EventSubscriber for ProstMessageEventSubscriber<M, F> where
     F: Fn(&M) {
     fn handle(&self, event_bytes: &Vec<u8>) {
         let event_bytes_clone: &[u8] = &mut event_bytes.clone();
-        let event = M::decode(event_bytes_clone).unwrap(); // unwrapやめる
-        (self.handle_prost_event)(&event);
+        let event = M::decode(event_bytes_clone);
+        match event {
+            Ok(event) =>
+                (self.handle_prost_event)(&event),
+            Err(_) => {
+                // dismiss
+            }
+        }
+
     }
 }
